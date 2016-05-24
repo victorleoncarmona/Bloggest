e12a6fbcba8195bdfabcb8f18148de31e96ee292
Víctor León <victor.leon.carmona@gmail.com>
2016-05-25 00:25:53 +0200

    Mostrando el detalle de un elemento

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,9 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+#Para hacer las queries de las bbdd
+from posts.models import Post
+
 
 # Create your views here.
 def post_create(request):
@@ -9,14 +13,19 @@ def post_create(request):
 	return render (request, "index.html",context)
 
 def post_detail(request): #retrive
+	#instance = Post.objects.get(id=300)  ¡Mejor la línea de abajo para controlar el error si no existe!
+	instance = get_object_or_404(Post, id=3)
 	context = {
-		"title": "Detail"
+		"title": instance.title,
+		"instance": instance,
 	}
-	return render (request, "index.html",context)
+	return render (request, "post_detail.html",context)
 
 def post_list(request):  #list items
 	if request.user.is_authenticated():
+		queryset = Post.objects.all()
 		context = {
+			"object_list": queryset,
 			"title": "My user List"
 		}
 	else:
